8c7eeedba951724d07739037eef7a909ec1d2ccb
Hassan Abouelela <hassan@hassanamr.com>
2025-09-20 22:19:00 +0400

    Add Backend Docker Configuration
    Signed-off-by: Hassan Abouelela <hassan@hassanamr.com>

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,3 +20,9 @@ app.add_middleware(
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app="app.main:app", host="0.0.0.0", port=8000)
