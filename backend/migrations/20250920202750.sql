924ebe80dd02346c1997f47098674af67c77cba1
Hassan Abouelela <hassan@hassanamr.com>
2025-09-21 03:11:40 +0400

    Fix Migrations To Match Models
    Signed-off-by: Hassan Abouelela <hassan@hassanamr.com>

--- backend/migrations/20250920202750.sql
+++ backend/migrations/20250920202750.sql
@@ -1,2 +1,3 @@
 -- Modify "users" table
-ALTER TABLE "public"."users" ADD COLUMN "password_hash" character varying NOT NULL DEFAULT 'invalid';
+ALTER TABLE "public"."users" ADD COLUMN "password_hash" character varying DEFAULT 'invalid' NOT NULL;
+ALTER TABLE "public"."users" ALTER COLUMN "password_hash" DROP DEFAULT;
